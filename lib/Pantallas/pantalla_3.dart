8a2558298533fb6436c2c1e886eb3c93f085be7a
festupinans <festupinans@unal.edu.co>
2021-11-09 00:04:02 -0500

    Final clase Lunes

--- lib/Pantallas/pantalla_3.dart
+++ lib/Pantallas/pantalla_3.dart
@@ -1,6 +1,15 @@
 import 'package:flutter/material.dart';
 
-class pantalla_3 extends StatelessWidget {
+class pantalla_3 extends StatefulWidget {
+  const pantalla_3({Key? key}) : super(key: key);
+
+  @override
+  _pantalla_3State createState() => _pantalla_3State();
+}
+
+class _pantalla_3State extends State<pantalla_3> {
+
+  String txt="";
 
   @override
   Widget build(BuildContext context) {
@@ -13,15 +22,31 @@ class pantalla_3 extends StatelessWidget {
           Container(
             padding: EdgeInsets.all(20.0),
             child: TextField(
+              onChanged: (texto){
+
+                setState(() {
+                  txt=texto;
+                });
+
+                print(txt);
+              },
+
               autofocus: false,
-                  keyboardType: TextInputType.number,
+              keyboardType: TextInputType.name,
+              textInputAction: TextInputAction.search,
+              decoration: InputDecoration(
+                  prefixIcon: Icon(Icons.account_box_outlined, color: Colors.pink),
+                  hintText: "Ingrese su nombre"
+              ),
             ),
           ),
           Center(
-            child: Text("Hola", style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold, color: Colors.pink),),
+            child: Text("Hola "+txt, style: TextStyle(fontSize: 20,fontWeight: FontWeight.bold, color: Colors.pink),),
           )
         ],
       ),
     );
   }
 }
+
+
